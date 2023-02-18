bb68ae208362dadfbd0cdba9e145b4daf8da4cac
ssung <qkrtjdcjf124@gmail.com>
2023-02-18 14:08:51 +0900

    외부 트랜잭션과 중첩 트랜잭션이 동작하는 원리에 대한 이해

--- src/test/kotlin/hello/springtx/propagation/BasicTxTest.kt
+++ src/test/kotlin/hello/springtx/propagation/BasicTxTest.kt
@@ -1,6 +1,8 @@
 package hello.springtx.propagation
 
 import hello.springtx.logInfo
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
@@ -8,6 +10,9 @@ import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
 import org.springframework.jdbc.datasource.DataSourceTransactionManager
 import org.springframework.transaction.PlatformTransactionManager
+import org.springframework.transaction.TransactionDefinition
+import org.springframework.transaction.UnexpectedRollbackException
+import org.springframework.transaction.interceptor.DefaultTransactionAttribute
 import org.springframework.transaction.support.DefaultTransactionDefinition
 import javax.sql.DataSource
 
@@ -68,5 +73,67 @@ class BasicTxTest {
         val tx2 = txManager.getTransaction(DefaultTransactionDefinition())
         logInfo("트랜잭션2 롤백")
         txManager.rollback(tx2)
+    }
+
+    @Test
+    fun outer_commit() {
+        logInfo("외부 트랜잭션 시작")
+        val outer = txManager.getTransaction(DefaultTransactionAttribute())
+        logInfo("outer.isNewTransaction()=${outer.isNewTransaction}")
+
+        logInfo("내부 트랜잭션 시작")
+        val inner = txManager.getTransaction(DefaultTransactionAttribute())
+        logInfo("inner.isNewTransaction()=${inner.isNewTransaction}")
+        logInfo("내부 트랜잭션 커밋")
+        txManager.commit(inner)
+
+        logInfo("외부 트랜잭션 커밋")
+        txManager.commit(outer)
+    }
+
+    @Test
+    fun outer_rollback() {
+        logInfo("외부 트랜잭션 시작")
+        val outer = txManager.getTransaction(DefaultTransactionAttribute())
+
+        logInfo("내부 트랜잭션 시작")
+        val inner = txManager.getTransaction(DefaultTransactionAttribute())
+        logInfo("내부 트랜잭션 커밋")
+        txManager.commit(inner)
+
+        logInfo("외부 트랜잭션 롤백")
+        txManager.rollback(outer)
+    }
+
+    @Test
+    fun inner_rollback() {
+        logInfo("외부 트랜잭션 시작")
+        val outer = txManager.getTransaction(DefaultTransactionAttribute())
+
+        logInfo("내부 트랜잭션 시작")
+        val inner = txManager.getTransaction(DefaultTransactionAttribute())
+        logInfo("내부 트랜잭션 롤백")
+        txManager.rollback(inner)
+
+        logInfo("외부 트랜잭션 커밋")
+        assertThatThrownBy { txManager.commit(outer) }.isInstanceOf(UnexpectedRollbackException::class.java)
+    }
+
+    @Test
+    fun inner_rollback_requires_new() {
+        logInfo("외부 트랜잭션 시작")
+        val outer = txManager.getTransaction(DefaultTransactionAttribute())
+        logInfo("outer.isNewTransaction()=${outer.isNewTransaction}")
+
+        logInfo("내부 트랜잭션 시작")
+        val defaultTransactionAttribute = DefaultTransactionAttribute()
+        defaultTransactionAttribute.propagationBehavior = TransactionDefinition.PROPAGATION_REQUIRES_NEW
+        val inner = txManager.getTransaction(defaultTransactionAttribute)
+        logInfo("inner.isNewTransaction()=${inner.isNewTransaction}")
+        logInfo("내부 트랜잭션 커밋")
+        txManager.commit(inner)
+
+        logInfo("외부 트랜잭션 커밋")
+        txManager.commit(outer)
     }
 }
